06322bd18bae84c4fedb9067347824f2b4d21704
Andrew Nadraliev <andrew.nadraliev@dsr-corporation.com>
2022-11-18 14:52:06 +0500

    Use null byte as a terminal operator when splitting the message

--- android/src/main/java/com/sicpa/ble/reactnative/MessageSplitter.kt
+++ android/src/main/java/com/sicpa/ble/reactnative/MessageSplitter.kt
@@ -6,11 +6,16 @@ import kotlin.math.min
 class MessageSplitter: DataSplitter {
 
     override fun chunk(message: ByteArray, index: Int, maxLength: Int): ByteArray? =
-        if (maxLength * (index - 1) >= message.size) {
+        if (maxLength * index == message.size) {
+            byteArrayOf(0x00)
+        } else if (maxLength * index > message.size) {
             null
-        } else if (maxLength * index >= message.size) {
-            " ".encodeToByteArray()
         } else {
             message.sliceArray((maxLength * index) until min(maxLength * (index + 1), message.size))
+                .let {
+                    if (maxLength * (index + 1) > message.size) {
+                        return@let it.toMutableList().apply { add(0x00) }.toByteArray()
+                    } else return@let it
+                }
         }
 }
