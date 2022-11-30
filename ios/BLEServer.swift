f2f63433a53e97bd481a23262006eefaaa30e16c
Andrew Nadraliev <andrew.nadraliev@dsr-corporation.com>
2022-11-30 18:02:46 +0300

    Fixed: send message completion was not called

--- ios/BLEServer.swift
+++ ios/BLEServer.swift
@@ -23,55 +23,58 @@ class BLEServer: NSObject, CBPeripheralManagerDelegate {
     private var inboundBuffer = ""
     
     private var advertiseCompletion: ((Result<Any?, BLEError>) -> Void)?
-    
+    private var sendMessageCompletion: ((Result<Any?, BLEError>) -> Void)?
+
     private var outboundQueue: [Data] = []
-    
+
     func isReady() -> Bool {
         return true
     }
-    
+
     func start() -> Void {
         // check the state to init bluetooth manager
         peripheralManager.state
     }
-    
+
     func stop() -> Void {
         // nothing to clean up
     }
-    
+
     func generateBleId(completion: @escaping (Result<String, BLEError>) -> Void) -> Void {
         completion(.success(String(Int.random(in: 1000..<10000))))
     }
-    
+
     func advertise(_ bleId: String, completion: @escaping (Result<Any?, BLEError>) -> Void) {
         advertiseCompletion = completion
-        
+
         if characteristic == nil {
             addServices()
         }
-        
+
         log(tag: BLEServer.tag, message: "Starting BLE advertise")
         peripheralManager.startAdvertising([CBAdvertisementDataLocalNameKey : bleId, CBAdvertisementDataServiceUUIDsKey: [SERVICE_ID]])
     }
-    
+
     func stopAdvertise() {
         peripheralManager.stopAdvertising()
     }
-    
+
     func sendMessage(message: String, completion: @escaping (Result<Any?, BLEError>) -> Void) {
         log(tag: BLEServer.tag, message: "Trying to send a message to central")
-        
+
+        sendMessageCompletion = completion
+
         guard let characteristic = characteristic else {
             let message = "Trying to send a message to central, but the server is not ready"
             log(tag: BLEServer.tag, message: message)
             completion(.failure(BLEError(message: message)))
             return
         }
-        
+
         onEventFired?(.init(type: .sendingMessage))
-        
+
         let chunks = splitMessage(data: Data(message.utf8), maxChunkLength: 180)
-        
+
         for i in chunks.indices {
             let result = peripheralManager.updateValue(chunks[i], for: characteristic, onSubscribedCentrals: nil)
             if !result {
@@ -80,8 +83,10 @@ class BLEServer: NSObject, CBPeripheralManagerDelegate {
                 break
             }
         }
-        
+
         if outboundQueue.isEmpty {
+            sendMessageCompletion = nil
+            completion(.success(nil))
             onEventFired?(.init(type: .messageSent))
         }
     }
@@ -90,12 +95,13 @@ class BLEServer: NSObject, CBPeripheralManagerDelegate {
         stopAdvertise()
         onClientDisconnected()
     }
-    
+
     private func onClientDisconnected() {
         inboundBuffer = ""
         outboundQueue.removeAll()
+        sendMessageCompletion = nil
     }
-    
+
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         switch peripheral.state {
         case .unknown:
@@ -114,17 +120,17 @@ class BLEServer: NSObject, CBPeripheralManagerDelegate {
             log(tag: BLEServer.tag, message: "Unknown State")
         }
     }
-    
+
     private func addServices() {
         log(tag: BLEServer.tag, message: "Adding services to our BLE peripheral")
-        
+
         let writableCharacteristic = CBMutableCharacteristic(type: CHARACTERISTIC_ID, properties: [.notify, .write, .read], value: nil, permissions: [.readable, .writeable])
         characteristic = writableCharacteristic
         let service = CBMutableService(type: SERVICE_ID, primary: true)
         service.characteristics = [writableCharacteristic]
         peripheralManager.add(service)
     }
-    
+
     func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
         if let error = error {
             log(tag: BLEServer.tag, message: "Could not start advertising", error: error)
@@ -136,24 +142,24 @@ class BLEServer: NSObject, CBPeripheralManagerDelegate {
             advertiseCompletion = nil
         }
     }
-    
+
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
         log(tag: BLEServer.tag, message: "Received write requests")
-        
+
         if requests.isEmpty {
             log(tag: BLEServer.tag, message: "Write requests list is empty")
             return
         }
-        
+
         if inboundBuffer.isEmpty {
             onEventFired?(.init(type: .startedMessageReceive))
         }
-        
+
         requests.forEach { request in
             handleWriteRequest(request: request)
         }
     }
-    
+
     func peripheralManagerIsReady(toUpdateSubscribers peripheral: CBPeripheralManager) {
         // peripheral is ready to send updates again, taking from the queue
         log(tag: BLEServer.tag, message: "Peripheral is ready to send updates again")
@@ -161,25 +167,27 @@ class BLEServer: NSObject, CBPeripheralManagerDelegate {
             log(tag: BLEServer.tag, message: "Characteristic is nil after sending some updates, this should not happen, something went terribly wrong")
             return
         }
-        
+
         var updateResult = false
-        
+
         repeat {
             if outboundQueue.isEmpty {
                 break
             }
-            
+
             let nextUpdate = outboundQueue.removeFirst()
             updateResult = peripheral.updateValue(nextUpdate, for: characteristic, onSubscribedCentrals: nil)
-            
+
             if !updateResult {
                 log(tag: BLEServer.tag, message: "Cannot send the update chunk, the internal queue is full, returning to our own queue")
                 outboundQueue.insert(nextUpdate, at: 0)
             }
-            
+
         } while !outboundQueue.isEmpty && updateResult
-        
+
         if outboundQueue.isEmpty {
+            sendMessageCompletion?(.success(nil))
+            sendMessageCompletion = nil
             onEventFired?(.init(type: .messageSent))
         }
     }
